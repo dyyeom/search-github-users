39826ef60f1ff181380464e1c7223c3fb6648e5b
Doyeong Yeom <dyyeom@gmail.com>
2018-12-01 16:00:00 +0900

    Initial Project.

---
+++ search-github-users/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  search-github-users
+//
+//  Created by Doyeong Yeom on 01/12/2018.
+//  Copyright © 2018 blueprog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
